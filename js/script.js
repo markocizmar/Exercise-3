d5c05438f288328d375f1738b1b297abffb08b3a
Marko Cizmar <m.cizmar@rs.emakina.com>
2021-11-08 16:27:09 +0100

    began responsivity

--- js/script.js
+++ js/script.js
@@ -18,3 +18,13 @@ function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
+
+//--------ACCORDION---------//
+
+const accordion = document.getElementsByClassName("appetisers");
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
