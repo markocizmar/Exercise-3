baf1db4008730487dcbe6819b53024b6b24f5dfb
Marko Cizmar <m.cizmar@emakina.rs>
2021-11-09 10:55:13 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//---------BURGER MENU----------//
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+//--------ACCORDION---------//
+
+const accordion = document.getElementsByClassName("appetisers");
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+}
